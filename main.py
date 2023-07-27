445362e60dc1e6bea5f8afe745adef0a6b93d747
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-27 09:13:41 -0400

    added Sky forecasting images

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ from backend import get_data
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5, help='Select number of forecasted days')
-option = st.selectbox('Select data to view', ('Tempature', 'Sky'))
+option = st.selectbox('Select data to view', ('Temperature', 'Sky'))
+
 st.subheader(f'{option} for the next {days} days in {place}')
 
-dates = ['2022-25-10', '2022-26-10']
-temperatures = [10, 11]
-temperatures = [days * i for i in temperatures]
+if place:
+    #set the temperature, sky data 
+    filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+    if option == 'Temperature':
+        temperatures = [dict['main']['temp'] for dict in filtered_data]
+        dates = [dict['dt_txt'] for dict in filtered_data]
+        #create temperature plot
+        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+        st.plotly_chart(figure)
 
-figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+    if option == 'Sky':
+        images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 
+                  'Raim': 'images/rain.png', 'Snow': 'images/snow.png'}
+        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width=115)
