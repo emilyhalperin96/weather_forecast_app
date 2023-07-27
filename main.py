dac6ac999dc3337fdeea3a7b94310788d6779b95
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-27 09:17:21 -0400

    fix error for instance where city does not exist

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ option = st.selectbox('Select data to view', ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
 if place:
-    #set the temperature, sky data 
-    filtered_data = get_data(place, days)
+    #try in case there is no place
+    try:
+        #set the temperature, sky data 
+        filtered_data = get_data(place, days)
 
-    if option == 'Temperature':
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        dates = [dict['dt_txt'] for dict in filtered_data]
-        #create temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
-        st.plotly_chart(figure)
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            #create temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
 
-    if option == 'Sky':
-        images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 
-                  'Raim': 'images/rain.png', 'Snow': 'images/snow.png'}
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == 'Sky':
+            images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 
+                    'Raim': 'images/rain.png', 'Snow': 'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write('That place does not exist.')
